bc990d8bc32f68b2ceeff622dd0eac67c16ff792
MadhuAmbat <madhuambatprof4442@gmail.com>
2023-03-22 17:20:46 +0530

    "After SSL"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularDatingApp';
+  users:any=null;
+
+  constructor(private http : HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:7172/api/Main").subscribe({
+      next:data=>{this.users = data},
+    } 
+    );
+  }
+
 }
